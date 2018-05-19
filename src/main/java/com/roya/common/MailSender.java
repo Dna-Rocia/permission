093876eb9b86111bf9558da5b98c8d37dce823ef
Loyaill <luo@6shangwang.com>
2018-05-19 14:14:45 +0800

    发送邮件模板

---
+++ src/main/java/com/roya/common/MailSender.java
@@ -0,0 +1,35 @@
+package com.roya.common;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by idea
+ * description :
+ *		邮件模板
+ * @author Loyaill
+ * @since 1.8JDK
+ * CreateDate 2018-05-18-13:36
+ */
+
+public class MailSender {
+
+		@Resource
+		private JavaMailSender mailSender;
+
+		public void send(SimpleMailMessage mail) {
+			mailSender.send(mail);
+		}
+
+		public JavaMailSender getMailSender(){
+			return this.mailSender;
+		}
+
+		public void setMailSender(JavaMailSender mailSender) {
+			this.mailSender = mailSender;
+		}
+
+
+}
